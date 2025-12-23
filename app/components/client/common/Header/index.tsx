ae904bdcadbd4f21e91ecbb1c0b31a6a6bb4b86f
rcepeda <riguxc@gmail.com>
2025-12-23 12:21:20 -0600

    feat: Refactor code structure for improved readability and maintainability

--- app/components/client/common/Header/index.tsx
+++ app/components/client/common/Header/index.tsx
@@ -2,8 +2,8 @@ import { Icon } from '@/app/components/icons';
 
 export const Header = () => {
 	return (
-		<header className='w-full min-h-4 flex bg-crypto-gray items-center p-3 border-b border-gray-400'>
-			<div className='flex flex-row gap-x-1 items-center'>
+		<header className='w-full min-h-4 flex bg-crypto-gray items-center p-3 border-b border-gray-400 fixed top-0 left-0 z-10'>
+			<div className='flex flex-row gap-x-1 items-center w-full max-w-2xl ml-auto mr-auto'>
 				<Icon.Brand />
 				<h1 className='font-light text-xs text-crypto-black'>CryptoApp</h1>
 			</div>
--- app/components/client/common/List/index.tsx
+++ app/components/client/common/List/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Icon } from '@/app/components/icons';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import { Children, isValidElement, MouseEvent, ReactElement } from 'react';
@@ -11,6 +11,7 @@ type TListItemProps = {
 	onClick?: (evt?: MouseEvent<HTMLLIElement>) => void;
 	isLink?: boolean;
 	link?: string;
+	endContent?: ReactElement;
 };
 const ItemBody = ({ title, subTitle = '', iconUrl }: Omit<TListItemProps, 'onCLick' | 'isLink' | 'link'>) => (
 	<>
@@ -21,23 +22,24 @@ const ItemBody = ({ title, subTitle = '', iconUrl }: Omit<TListItemProps, 'onCLi
 				<p className='text-xs font-light uppercase'>{subTitle}</p>
 			</div>
 		</div>
-		<Icon.ArrowRight />
 	</>
 );
 
-const Item = ({ title, subTitle = '', iconUrl, onClick = () => {}, isLink = false, link = '' }: TListItemProps) => {
+const Item = ({ title, subTitle = '', iconUrl, onClick = () => {}, isLink = false, link = '', endContent }: TListItemProps) => {
 	const className = 'flex flex-row w-full p-4 bg-white rounded-xl hover:bg-gray-100 hover:cursor-pointer transition duration-300 text-crypto-black active:scale-98';
 
 	if (isLink) {
 		return (
 			<Link href={link} className={className}>
 				<ItemBody title={title} subTitle={subTitle} iconUrl={iconUrl} />
+				{endContent && endContent}
 			</Link>
 		);
 	}
 	return (
 		<li className={className} onClick={onClick} role='button'>
 			<ItemBody title={title} subTitle={subTitle} iconUrl={iconUrl} />
+			{endContent && endContent}
 		</li>
 	);
 };
@@ -45,17 +47,19 @@ const Item = ({ title, subTitle = '', iconUrl, onClick = () => {}, isLink = fals
 type TListContentProps = {
 	title?: string;
 	children: ReactElement<typeof Item> | ReactElement<typeof Item>[];
+	type: 'grid' | 'column';
 };
-const Content = ({ title, children }: TListContentProps) => {
+const Content = ({ title, children, type = 'column' }: TListContentProps) => {
 	Children.forEach(children, child => {
 		if (!isValidElement(child) || child.type !== Item) {
 			throw new Error('List.Content only uses List.Item as children');
 		}
 	});
+	const listClassName = type === 'grid' ? 'w-full grid grid-cols-2 gap-4' : 'w-full flex flex-col gap-y-2';
 	return (
 		<div className='flex flex-col gap-y-2'>
 			{title && <h3 className='text-md font-medium'>{title}</h3>}
-			<ul className='w-full flex flex-col gap-y-2'>{children}</ul>
+			<ul className={listClassName}>{children}</ul>
 		</div>
 	);
 };
