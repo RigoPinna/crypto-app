e4a0b21c7b890a7e3f743fe6d5a85252da3d0d89
rcepeda <riguxc@gmail.com>
2025-12-24 14:17:41 -0600

    feat: Implement CryptoDetailsPage component with price and market data

---
+++ app/[id]/page.tsx
@@ -0,0 +1,70 @@
+import Image from 'next/image';
+import { PRICE_CHART, SINGLE_CRYPTO_MOCK } from '../__mocks__/cryptoData';
+import { Chip, DescriptionText, ExploreLinkSection, Header, PriceChart } from '../components/client';
+import { Icon } from '../components/icons';
+
+const CryptoDetailsPage = () => {
+	const crypto = SINGLE_CRYPTO_MOCK;
+	const chart = PRICE_CHART;
+	const chartData = chart.prices.map(([timestamp, price]) => ({
+		time: new Date(timestamp).toLocaleTimeString(),
+		price,
+	}));
+
+	return (
+		<>
+			<Header />
+			<main className='pt-16 pb-6 px-3 flex flex-col gap-y-6'>
+				<section className='w-full'>
+					<div className='flex flex-row gap-x-0.5 items-center'>
+						<Image src={crypto.image.large} alt={crypto.name} width={34} height={34} className='rounded-full' />
+						<h1 className='text-lg font-semibold text-crypto-black'>{crypto.name}</h1>
+					</div>
+					<p className='text-2xl font-bold text-crypto-black'>${crypto.market_data.current_price.usd.toLocaleString()}</p>
+					<p className={`text-sm ${crypto.market_data.price_change_percentage_24h > 0 ? 'text-green-500' : 'text-red-500'}`}>
+						{crypto.market_data.price_change_percentage_24h.toFixed(2)}%
+					</p>
+				</section>
+				<section>
+					<PriceChart data={chartData} />
+				</section>
+				<section className='border-t border-t-crypto-black-200 pb-2 pt-4'>
+					<div className='grid grid-cols-2 gap-y-4 text-sm'>
+						<div className='border-b border-b-crypto-black-200 pb-2'>
+							<p className='text-gray-500 uppercase'>24H Vol</p>
+							<p className='font-medium text-crypto-black text-xs'>${crypto.market_data.total_volume.usd.toLocaleString()}</p>
+						</div>
+						<div className='border-b border-b-crypto-black-200 pb-2'>
+							<p className='text-gray-500 uppercase'>Market Cap</p>
+							<p className='font-medium text-crypto-black text-xs'>${crypto.market_data.market_cap.usd.toLocaleString()}</p>
+						</div>
+						<div className='border-b border-b-crypto-black-200 pb-2'>
+							<p className='text-gray-500 uppercase'>Circulating Supply</p>
+							<p className='font-medium text-crypto-black text-xs'>{crypto.market_data.circulating_supply.toLocaleString()}</p>
+						</div>
+						<div className='border-b border-b-crypto-black-200 pb-2'>
+							<p className='text-gray-500 uppercase'>Max Supply</p>
+							<p className='font-medium text-crypto-black text-xs'>{crypto.market_data.max_supply ?? '∞'}</p>
+						</div>
+					</div>
+				</section>
+				<section>
+					<h3 className='text-lg font-semibold text-crypto-black mb-1'>About {crypto.name}</h3>
+					<DescriptionText className='text-xs' text={crypto.description.en} />
+				</section>
+
+				<section>
+					<h3 className='text-lg font-semibold text-crypto-black mb-1'>Categories</h3>
+					<div className='flex flex-row flex-wrap gap-x-1 gap-y-2'>
+						{crypto.categories.map((category, i) => (
+							<Chip key={`${crypto.id}${i}category`} text={category} />
+						))}
+					</div>
+				</section>
+				<ExploreLinkSection links={crypto?.links?.blockchain_site || []} />
+			</main>
+		</>
+	);
+};
+
+export default CryptoDetailsPage;
