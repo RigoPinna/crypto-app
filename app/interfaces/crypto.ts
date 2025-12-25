99d5c87fc92961ed8a2102d2d8e5d28a03946bdd
rcepeda <riguxc@gmail.com>
2025-12-24 19:04:40 -0600

    feat: Add search functionality with ISearchResult interface and searchCrypto API method

--- app/interfaces/crypto.ts
+++ app/interfaces/crypto.ts
@@ -1715,3 +1715,39 @@ export interface ICryptoChartData {
 	market_caps: [number, number][];
 	total_volumes: [number, number][];
 }
+export interface ISearchResult {
+	coins: Coin[];
+	exchanges: Exchange[];
+	categories: Category[];
+	nfts: Nft[];
+}
+
+export interface Coin {
+	id: string;
+	name: string;
+	api_symbol: string;
+	symbol: string;
+	market_cap_rank: number;
+	thumb: string;
+	large: string;
+}
+
+export interface Exchange {
+	id: string;
+	name: string;
+	market_type: string;
+	thumb: string;
+	large: string;
+}
+
+export interface Category {
+	id: string;
+	name: string;
+}
+
+export interface Nft {
+	id: string;
+	name: string;
+	symbol: string;
+	thumb: string;
+}
