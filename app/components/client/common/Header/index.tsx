fdc7ef747339c1f6dd680959c93a41db0e95a1f1
rcepeda <riguxc@gmail.com>
2025-12-24 19:06:01 -0600

    feat: Refactor Header component to accept dynamic content via props

--- app/components/client/common/Header/index.tsx
+++ app/components/client/common/Header/index.tsx
@@ -1,11 +1,16 @@
-import { Icon } from '@/app/components/icons';
-
-export const Header = () => {
+import { ReactElement } from 'react';
+type THeaderProps = {
+	startContent: ReactElement;
+	centerContent?: ReactElement;
+	endContent?: ReactElement;
+};
+export const Header = ({ startContent, centerContent, endContent }: THeaderProps) => {
 	return (
-		<header className='w-full min-h-4 flex bg-crypto-gray items-center p-3 border-b border-gray-400 fixed top-0 left-0 z-10'>
-			<div className='flex flex-row gap-x-1 items-center w-full max-w-2xl ml-auto mr-auto'>
-				<Icon.Brand />
-				<h1 className='font-light text-xs text-crypto-black'>CryptoApp</h1>
+		<header className='w-full min-h-4 flex bg-crypto-gray/30 backdrop-blur-md items-center fixed top-0 left-0 z-10 shadow-lg shadow-gray-300/30'>
+			<div className='py-3 flex flex-row gap-x-1 items-center w-full max-w-2xl ml-auto mr-auto'>
+				<div className='flex items-center w-[33%] h-8 p-3'>{startContent}</div>
+				<div className='flex items-center justify-center flex-1 h-8'>{centerContent && centerContent}</div>
+				<div className='pr-3 flex items-center justify-end w-[33%] h-8'>{endContent && endContent}</div>
 			</div>
 		</header>
 	);
