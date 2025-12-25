c335d0c7e8bb524557f68af64777c89a9e9995cd
rcepeda <riguxc@gmail.com>
2025-12-25 11:57:24 -0600

    feat: Added new prop to go to specific page

--- app/components/client/common/BackButton/index.tsx
+++ app/components/client/common/BackButton/index.tsx
@@ -3,9 +3,16 @@ import { Icon } from '@/app/components/icons';
 import { Button } from '../Button';
 import { useRouter } from 'next/navigation';
 
-export const BackButton = () => {
+type TBackButtonProps = {
+	backTo?: string;
+};
+export const BackButton = ({ backTo }: TBackButtonProps) => {
 	const router = useRouter();
 	const onBack = () => {
+		if (backTo) {
+			router.push(backTo);
+			return;
+		}
 		router.back();
 	};
 	return (
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -4,7 +4,7 @@ import { Icon } from '../components/icons';
 const SearchPage = () => {
 	return (
 		<>
-			<Header startContent={<BackButton />} centerContent={<Icon.Brand />} />
+			<Header startContent={<BackButton backTo='/' />} centerContent={<Icon.Brand />} />
 
 			<main className='pt-16 pb-6 px-3'>
 				<h3 className='text-xs text-crypto-black mb-2'>Search by</h3>
