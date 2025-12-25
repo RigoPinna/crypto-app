7c30fbc89e89c44de0a02a0484f0dd2b53d51835
rcepeda <riguxc@gmail.com>
2025-12-25 12:08:43 -0600

    feat: Added suspense

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,4 +1,5 @@
-import { BackButton, Header, SearchForm, SearchResult } from '../components/client';
+import { Suspense } from 'react';
+import { BackButton, Header, SearchForm, SearchResult, Spinner } from '../components/client';
 import { Icon } from '../components/icons';
 
 const SearchPage = () => {
@@ -8,8 +9,10 @@ const SearchPage = () => {
 
 			<main className='pt-16 pb-6 px-3'>
 				<h3 className='text-xs text-crypto-black mb-2'>Search by</h3>
-				<SearchForm />
-				<SearchResult />
+				<Suspense fallback={<Spinner />}>
+					<SearchForm />
+					<SearchResult />
+				</Suspense>
 			</main>
 		</>
 	);
